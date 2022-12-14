2b6032119d7f340a7bb00cc27379503a4d03e687
jaeyeong815 <jae0ee@naver.com>
2022-12-14 18:13:28 +0900

    feat: 로그인 form 레이아웃

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+
 const Auth = () => {
-  return <div>Auth</div>;
+  const [loginData, setLoginData] = useState({ email: '', password: '' });
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={onChangeHandler}
+          value={loginData.email}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={onChangeHandler}
+          value={loginData.password}
+          required
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Google Log in</button>
+        <button>Github Log in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
