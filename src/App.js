b0de1e633a06f3058cbc631f98d012db5b9dbc8b
jaeyeong815 <jae0ee@naver.com>
2022-12-14 17:55:37 +0900

    feat: firebase auth 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import Router from 'Router';
+import { authService } from 'fbase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <div>
       <Router isLoggedIn={isLoggedIn} />
--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
@@ -13,3 +14,4 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+export const authService = getAuth(firebaseApp);
