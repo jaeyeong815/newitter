1ac29e730775485aa97443ecb6dbc21261fc963f
jaeyeong815 <jae0ee@naver.com>
2022-12-20 22:32:27 +0900

    feat: 업로드한 사진 미리보기

--- src/components/Main/Newitter.js
+++ src/components/Main/Newitter.js
@@ -5,12 +5,26 @@ import { addNewit, getNewits } from 'utils/fbase/newitFbase';
 const Newitter = ({ user }) => {
   const [newitText, setNewitText] = useState('');
   const [newits, setNewits] = useState([]);
+  const [imgUrl, setImgUrl] = useState(null);
 
   useEffect(() => {
     getNewits(setNewits);
   }, []);
 
   const onChange = (e) => setNewitText(e.target.value);
+
+  const onFileChange = (e) => {
+    const { files } = e.target;
+    const img = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const { result } = finishedEvent.currentTarget;
+      setImgUrl(result);
+    };
+    reader.readAsDataURL(img);
+  };
+
+  const onClearFile = () => setImgUrl(null);
 
   const onsubmit = (e) => {
     e.preventDefault();
@@ -28,7 +42,14 @@ const Newitter = ({ user }) => {
           value={newitText}
           onChange={onChange}
         />
+        <input type='file' accept='image/*' onChange={onFileChange} />
         <button type='submit'>Newit!</button>
+        {imgUrl && (
+          <div>
+            <img src={imgUrl} alt='미리보기' width='50px' height='50px' />
+            <button onClick={onClearFile}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {newits.map((newit) => (
